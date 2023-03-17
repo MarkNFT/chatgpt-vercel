a1975523b68bcad8b1c83f2302d7ec4239689889
MarkNFT <103547118+MarkNFT@users.noreply.github.com>
2023-03-17 12:07:44 +0800

    Update system.ts
    Change tip

--- src/system.ts
+++ src/system.ts
@@ -7,10 +7,10 @@ export const setting = {
   systemRule: ""
 }
 
-export const message = `- 由 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 和 [Vercel](http://vercel.com/) 提供支持。
-- 由 [@ourongxing](https://github.com/ourongxing) 基于 [chatgpt-demo](https://github.com/ddiu8081/chatgpt-demo) 开发，查看 [源码](https://github.com/ourongxing/chatgpt-vercel)，欢迎自部署。
-- 该网站仅作为演示，不提供长期服务，网站使用\`密码\`请至公众号[马克云](https://mp.weixin.qq.com/s/t8Hmk7JOvel7_pi5I5Vhpg)获取。
-- [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] Prompt 预设。[[↑]] 可编辑最近一次提问。点击名称滚动到顶部，点击输入框滚动到底部。`
+export const message = `- 网站使用\`密码\`请至公众号[马克云](https://mp.weixin.qq.com/s/t8Hmk7JOvel7_pi5I5Vhpg)获取，打开⚙️填写。
+- [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] Prompt 预设。[[↑]] 可编辑最近一次提问。点击名称滚动到顶部，点击输入框滚动到底部。
+- 由 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 和 [Vercel](http://vercel.com/) 提供支持。
+- 由 [@ourongxing](https://github.com/ourongxing) 基于 [chatgpt-demo](https://github.com/ddiu8081/chatgpt-demo) 开发，查看 [源码](https://github.com/ourongxing/chatgpt-vercel)，欢迎自部署。`
 
 export type Setting = typeof setting
 
